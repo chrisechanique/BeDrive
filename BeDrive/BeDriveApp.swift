495df54301316e1b97aab652f9374c69b8b4e436
chrisechanique <caechanique@gmail.com>
2023-11-20 16:46:28 +0100

    Initial Commit

---
+++ BeDrive/BeDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeDriveApp.swift
+//  BeDrive
+//
+//  Created by Chris Echanique on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BeDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
